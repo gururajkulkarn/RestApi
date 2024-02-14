324c59da1d390cf1412230f81bdaf8d41738b520
gururajkulkarn <gururajkulkarni115@gmail.com>
2024-02-14 11:40:08 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const users = require('./users'); // Adjust the path as needed
+const app = express();
+const port = 3001;
+
+app.use(express.json());
+
+// Get all users
+app.get('/api/users', (req, res) => {
+  res.json({ users });
+});
+
+// Get a specific user by ID
+app.get('/api/users/:id', (req, res) => {
+    const userId = parseInt(req.params.id);
+    const user = users.find(u => u.id === userId);
+  
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+  
+    res.json({ user });
+  });
+  
+
+// Add a new user
+app.post('/api/users', (req, res) => {
+  const userData = req.body;
+  const newUser = { id: users.length + 1, ...userData };
+  users.push(newUser);
+
+  res.status(201).json({ user: newUser });
+});
+
+// Update a user by ID
+app.put('/api/users/:id', (req, res) => {
+  const userId = parseInt(req.params.id);
+  const userIndex = users.findIndex(u => u.id === userId);
+
+  if (userIndex === -1) {
+    return res.status(404).json({ error: 'User not found' });
+  }
+
+  users[userIndex] = { ...users[userIndex], ...req.body };
+
+  res.json({ user: users[userIndex] });
+});
+
+// Delete a user by ID
+app.delete('/api/users/:id', (req, res) => {
+  const userId = parseInt(req.params.id);
+  users = users.filter(u => u.id !== userId);
+
+  res.json({ message: 'User deleted successfully' });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
---
+++ users.js
@@ -0,0 +1,21 @@
+// usersData.js
+const users = [
+    { id: 1, name: 'gururaj kulkarni', age: 26 },
+    { id: 2, name: 'Deepak kulkarni', age: 28 },
+    { id: 3, name: 'Ravi ', age: 44 },
+    { id: 4, name: 'Akash ', age: 11 },
+    { id: 5, name: 'Anil ', age: 16 },
+    { id: 6, name: 'Raju ', age: 18 },
+    { id: 7, name: 'Kiran ', age: 26 },
+    { id: 8, name: 'Veeresh ', age: 38 },
+    { id: 9, name: 'RAGU ', age: 56 },
+    { id: 10, name: 'Ramesh', age: 48 },
+    { id: 11, name: 'Avinash ', age: 22 },
+    { id: 12, name: 'Anirudh ', age: 14 },
+    { id: 13, name: 'Danish ', age: 11 },
+    { id: 14, name: 'Ravi ', age: 12 },
+    { id: 15, name: 'Rakesh ', age: 17 },
+  ];
+  
+  module.exports = users;
+  
